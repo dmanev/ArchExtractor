9cf6419e15d70e8893d778b1a002e55aff92ccd0
Dimitar Manev <dmanev@gmail.com>
2014-08-19 20:32:32 +0300

    #2 Work-breakdown created; Minor formating corrections applied on Diplomna.odt

--- ArchExtractor/ProjMngt/ArchExtractor Tasks.mm
+++ ArchExtractor/ProjMngt/ArchExtractor Tasks.mm
@@ -1,4 +1,97 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1408459269963" ID="ID_756035737" MODIFIED="1408461111307" TEXT="ArchExtractor Tasks"/>
+<node CREATED="1408459269963" ID="ID_756035737" MODIFIED="1408461758838" TEXT="ArchExtractor Tasks">
+<node COLOR="#006633" CREATED="1408461672441" ID="ID_940227126" MODIFIED="1408465320245" POSITION="right" STYLE="fork" TEXT="Project Management">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#338800" CREATED="1408468705383" ID="ID_895288911" MODIFIED="1408469013442" TEXT="Create work-breakdown analysis">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node COLOR="#990000" CREATED="1408468723787" ID="ID_1051024860" MODIFIED="1408468745731" TEXT="Fill-in Tasks in GitHub">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+<node COLOR="#006633" CREATED="1408461698834" ID="ID_80881935" MODIFIED="1408465320243" POSITION="right" STYLE="fork" TEXT="Design Tasks">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1408464194705" ID="ID_283648968" MODIFIED="1408465853267" TEXT="Create major sequence diagrams">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408468576663" ID="ID_941772665" MODIFIED="1408468594399" TEXT="Analyse what diagrams are missed and create them">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#669900" CREATED="1408463135211" ID="ID_754635155" MODIFIED="1408467347513" STYLE="fork" TEXT="Diploma documentation writing">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#669900" CREATED="1408467353630" ID="ID_1554163198" MODIFIED="1408467366074" STYLE="fork" TEXT="Quality based">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1408466838754" ID="ID_304912270" MODIFIED="1408467347512" TEXT="Install MS Office and convert LibreOffice files to MS formats">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408466900714" ID="ID_336481484" MODIFIED="1408467347512" TEXT="Double check the document for copy-paste and translation glitches">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408467290636" ID="ID_241321981" MODIFIED="1408467347513" TEXT="Double check document look &amp; feel. Apply corrections if needed">
+<icon BUILTIN="yes"/>
+<node COLOR="#999999" CREATED="1408467600862" ID="ID_1305288825" MODIFIED="1408468088071" TEXT="Are notations filled in. If any missed notation - fill it in.">
+<font NAME="SansSerif" SIZE="10"/>
+<icon BUILTIN="edit"/>
+</node>
+<node COLOR="#999999" CREATED="1408468032371" ID="ID_1649807722" MODIFIED="1408468065648" TEXT="Double check for missings in &quot;Used Literature section&quot;">
+<font NAME="SansSerif" SIZE="10"/>
+<icon BUILTIN="edit"/>
+</node>
+<node COLOR="#999999" CREATED="1408468104496" ID="ID_442837978" MODIFIED="1408468123077" TEXT="Check if there is complete Appendix included">
+<font NAME="SansSerif" SIZE="10"/>
+<icon BUILTIN="edit"/>
+</node>
+</node>
+</node>
+<node COLOR="#669900" CREATED="1408467376956" ID="ID_928335063" MODIFIED="1408467382757" STYLE="fork" TEXT="Content based">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1408467520409" ID="ID_1524977373" MODIFIED="1408468757357" TEXT="Fill-in Front Page details">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408467816027" ID="ID_769622663" MODIFIED="1408468762028" TEXT="Fill-in &quot;4. Methods/Models/Technologies&quot;">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408467877644" ID="ID_1427263388" MODIFIED="1408468765844" TEXT="Fill-in &quot;5. Solution&quot;">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408467921895" ID="ID_420728918" MODIFIED="1408468769216" TEXT="Fill-in &quot;6. Results and analysis&quot;">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408467992741" ID="ID_1890612063" MODIFIED="1408468772501" TEXT="Fill-in &quot;7. Wrap-up and future work&quot;">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#006633" CREATED="1408461689314" ID="ID_1428290027" MODIFIED="1408465320240" POSITION="right" STYLE="fork" TEXT="Development">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#669900" CREATED="1408463096164" ID="ID_1057490698" MODIFIED="1408465341644" STYLE="fork" TEXT="SW Implementation">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1408464041754" ID="ID_1397002824" MODIFIED="1408465761449" STYLE="fork" TEXT="Make generated XMI fully readable by Eclipse">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#669900" CREATED="1408463119166" ID="ID_1234657054" MODIFIED="1408465719998" STYLE="fork" TEXT="Code Generator">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1408465659329" ID="ID_865136060" MODIFIED="1408465709201" TEXT="Create Acceleo project">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408465674564" ID="ID_179926375" MODIFIED="1408465711870" TEXT="Create header file template">
+<icon BUILTIN="yes"/>
+</node>
+<node COLOR="#990000" CREATED="1408465721030" ID="ID_929829084" MODIFIED="1408465739697" TEXT="Create implementation file template">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1408467456343" ID="ID_59187852" MODIFIED="1408467479198" TEXT="Create AUTOSAR parser artefacts (demo level)">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+</node>
+<node COLOR="#006633" CREATED="1408461704519" ID="ID_816670732" MODIFIED="1408465320238" POSITION="right" STYLE="fork" TEXT="Testing &amp; Verification">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
 </map>
