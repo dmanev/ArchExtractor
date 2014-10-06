267b289a35393956188e7883adeacb82341d00b5
Dimitar Manev <dmanev@gmail.com>
2014-10-06 17:49:43 +0300

    #12 Restructured BoUML model

---
+++ ArchExtractor/model/295663_111.d
@@ -0,0 +1,62 @@
+format 74
+
+packagecanvas 128111 
+  package_ref 155119 // Base
+   color lightorange  xyzwh 141 62 2000 315 493
+end
+packagecanvas 128239 
+  package_ref 174831 // Analyzer
+   color red  xyzwh 162 119 2005 267 83
+end
+packagecanvas 128367 
+  package_ref 161775 // SwComponent
+   color gold  xyzwh 163 338 2005 125 83
+end
+packagecanvas 128495 
+  package_ref 168303 // Infrastructure
+   color gold  xyzwh 304 338 2005 125 83
+end
+packagecanvas 128623 
+  package_ref 174959 // Parser
+   color darkgreen  xyzwh 162 226 2005 267 83
+end
+packagecanvas 128751 
+  package_ref 201711 // ModelConverter
+   color darkorange  xyzwh 162 446 2005 267 83
+end
+packagecanvas 128879 
+  package_ref 148463 // Lib
+   color midmagenta  xyzwh 35 60 2010 91 493
+end
+packagecanvas 129007 
+  package_ref 154991 // Specific
+   color lightmediumblue  xyzwh 473 62 2000 483 495
+end
+packagecanvas 129135 
+  package_ref 155247 // STK
+   color mediumblue  xyzwh 494 117 2005 263 323
+end
+packagecanvas 129263 
+  package_ref 175087 // StkParser
+   color mediumgreen  xyzwh 510 226 2010 231 83
+end
+packagecanvas 129391 
+  package_ref 175215 // StkDataTypes
+   color mediumyellow  xyzwh 636 338 2010 105 83
+end
+packagecanvas 129519 
+  package_ref 188527 // StkPortInterfaces
+   color mediumyellow  xyzwh 509 338 2010 111 83
+end
+note 129647 "Criteria set for any
+standard architecture "
+  color lightmagenta  font large  xyzwh 777 117 2005 147 323
+packagecanvas 129903 
+  package_ref 128367 // ArchExtractor
+    xyzwh 16 14 1995 959 561
+end
+packagecanvas 130031 
+  package_ref 208239 // Base code generation
+   color green  xyzwh 16 589 2000 959 119
+end
+end
