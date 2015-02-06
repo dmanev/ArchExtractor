bcf43fa9684c21296ef1d346de29035ce19dc572
dmanev <dimitar.manev@strypes.eu>
2015-02-07 00:20:28 +0200

    #12 Gui resizing improved

--- ArchExtractor/bin/ArchExtractorGui.py
+++ ArchExtractor/bin/ArchExtractorGui.py
@@ -6,39 +6,59 @@ Created on Nov 10, 2014
 '''
 
 import os
-import Tkinter
+import Tkinter as tk
+import tkFont
 import tkFileDialog
 from ArchExtractor import ArchExtractor
 
 
-class ArchExtractorGui(Tkinter.Tk):
-    def __init__(self, parent):
-        Tkinter.Tk.__init__(self, parent)
-        self.parent = parent
-        self.title('ArchExtractor')
-        self.grid()
-        self.entry = Tkinter.Entry(self)
-        self.entry.grid(column=0, row=0, sticky='EW', columnspan=2)
+class ArchExtractorGui(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.master.title('ArchExtractor')
+        self.pack(fill="both", expand=True)
+        self.customFont = tkFont.Font(family="Verdana", size=14)
+#         self.bind('<Up>', self.onFontBigger())
+        self.createWidgets()
+
+#     def onFontBigger(self):
+#         size = self.customFont['size']
+#         self.customFont.configure(size=size+2)
+
+    def createWidgets(self):
+        top_frame = tk.Frame(self)
+        button_frame = tk.Frame(self)
+        top_frame.pack(side="top", fill="x")
+        button_frame.pack(side="bottom", fill="both", expand=True)
+
+        self.entry = tk.Entry(top_frame, font=self.customFont)
+        self.entry.pack(side="top", fill="x")
         self.entry.insert(0, os.getcwd())
-        self.buttonGetDir = Tkinter.Button(self,
-                                           text=u"Get Project Directory",
-                                           command=self.askDirectory)
-        self.buttonGetDir.grid(column=0, row=2)
-        self.buttonAnalyze = Tkinter.Button(self,
-                                            text=u"Analyze",
-                                            command=self.analyzeProject)
-        self.buttonAnalyze.grid(column=1, row=2)
-        self.label = Tkinter.Label(self,
-                                   text="Select Project directory",
-                                   anchor="w",
-                                   fg="white",
-                                   bg="blue")
-        self.label.grid(column=0, row=1, columnspan=2, sticky='EW')
+
+        self.label = tk.Label(top_frame,
+                              text="Select Project directory",
+                              anchor="w",
+                              fg="white",
+                              bg="blue",
+                              font=self.customFont)
+        self.label.pack(side="top", fill="x")
+
+        self.buttonGetDir = tk.Button(button_frame,
+                                      text=u"Get Project Directory",
+                                      command=self.askDirectory,
+                                      font=self.customFont)
+        self.buttonGetDir.pack(side="left", fill="both", expand=True)
+
+        self.buttonAnalyze = tk.Button(button_frame,
+                                       text=u"Analyze",
+                                       command=self.analyzeProject,
+                                       font=self.customFont)
+        self.buttonAnalyze.pack(side="left", fill="both", expand=True)
 
     def askDirectory(self):
         dirPath = tkFileDialog.askdirectory(parent=self,
                                             title='Choose directory')
-        self.entry.delete(0, Tkinter.END)
+        self.entry.delete(0, tk.END)
         self.entry.insert(0, dirPath)
         return dirPath
 
@@ -62,5 +82,5 @@ class ArchExtractorGuiException(Exception):
 
 
 if __name__ == '__main__':
-    app = ArchExtractorGui(None)
+    app = ArchExtractorGui(tk.Tk())
     app.mainloop()
